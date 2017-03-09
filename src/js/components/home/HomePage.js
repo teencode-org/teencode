e367986c714ffd9358e11bfeb4385fc7aeb3bc3d
Innocent <innocent.amadi@andela.com>
2017-03-09 13:11:33 +0100

    refractor text on home screen.

--- src/js/components/home/HomePage.js
+++ src/js/components/home/HomePage.js
@@ -10,8 +10,8 @@ class HomePage extends React.Component {
    render () {
      return (
        <div className="jumbotron">
-        <h1>React + Bootstrap + Test tutorial by Cent</h1>
-        <p>Setting up development environment, routing and tests</p>
+        <h1>Welcome to Teencode's really amazing site</h1>
+        <p>Empowering the future generation of Africa's tech leaders</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
        </div>
      )
