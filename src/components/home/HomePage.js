af9f32dd8c5607661f8ed8ba53e645f424a5c56e
Tijesunimi Peters <tijesunimi.peters@andela.com>
2017-03-09 10:10:55 +0100

    Added the coming soon page

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class HomePage extends React.Component {
+  constructor(props, context){
+    super(props)
+
+   }
+
+   render () {
+     return (
+      <div className="container">
+       <div className="jumbotron">
+        <h1>Coming Soon</h1>
+       </div>
+      </div>
+     )
+   }
+ }
+ export default HomePage
