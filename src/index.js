38e259fd1dee3a53232151adf6a563284cd4a839
Tijesunimi Peters <tijesunimi48@gmail.com>
2017-03-09 13:37:29 +0100

    Revert "add production scripts, refractor file system"

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import '../css/styles.css';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 render (
   <Router history={browserHistory} routes={routes} />,
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,76 +1,36 @@
 import webpack from 'webpack';
 import path from 'path';
 
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
-const buildPath = path.resolve(__dirname, 'dist');
-const entryPath = path.resolve(__dirname, 'src', 'js', 'index.js');
-
-module.exports = {
+export default {
   debug: true,
-  devtool: '#inline-source-map',
+  devtool: 'cheap-module-eval-source-map',
   noInfo: false,
   entry: [
-    'eventsource-polyfill',
-    'webpack-hot-middleware/client?reload=true',
-    entryPath
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+    './src/index'
   ],
-  target: 'web',
+  target: 'web', // bundle app the way web browsers can understand
   output: {
-    path: buildPath,
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
     contentBase: './src'
   },
-  module: {
-    loaders: [
-      {
-        test: /src\/js\/.+.js$/,
-        exclude: /node_modules/,
-        loader: 'babel'
-      },
-      // SASS
-      {
-        test: /css\/.+.(scss|css)$/,
-        loaders: ['style', 'css', 'sass']
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url?limit=25000'
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
-    ]
-  },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
-  resolve: {
-    modulesDirectories: [
-      'src',
-      'node_modules'
-    ],
-    extensions: ['', '.json', '.js', '.jsx']
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
   }
 };
