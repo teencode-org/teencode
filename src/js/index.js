893cfdfc3512fc730ae1cc5d4495ad40dd80ff64
Innocent <innocent.amadi@andela.com>
2017-03-19 16:19:44 +0100

    fix conflicts with master merge

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-<<<<<<< HEAD:src/js/index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery';
 import 'tether';
 import 'bootstrap/dist/js/bootstrap.min';
 import '../scss/main.scss';
-=======
-import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
->>>>>>> develop:src/index.js
 
 render (
   <Router history={browserHistory} routes={routes} />,
