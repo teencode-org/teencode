6c2b43383c9629e8b02ba41a1ed2c0e3b4f3a0af
Chineze Nwosu <chineze.nwosu@andela.com>
2017-03-21 09:11:19 +0100

    make page responsive, fix navbar sticky-top

--- src/js/components/common/Header.js
+++ src/js/components/common/Header.js
@@ -1,30 +1,70 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 import logo from '../../../img/logo.png';
+import utils from '../../utils';
 
-const Header = () => {
-  return (
-    <nav id="top-nav" className="navbar navbar-full navbar-custom navbar-fixed-top" data-toggle="sticky-onscroll" data-offset-top="100">
-      <div className="container">
-        <div className="navbar-header">
-          <button className="navbar-toggler hidden-md-up pull-xs-right pull-sm-right" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-            &#9776;
-          </button>
-          <Link to="#page-top" className="navbar-brand page-scroll"><img src={logo}/></Link>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.stickyToggle = this.stickyToggle.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.handleScroll);
+  }
+
+  stickyToggle(sticky) {
+    const stickyHeight = sticky.clientHeight;
+    const stickyTop = sticky.offsetTop;
+    const scrollTop = document.body.scrollTop;
+
+    if (scrollTop > stickyHeight + stickyTop){
+      utils.addClass(sticky, 'is-sticky');
+    } else if (scrollTop <= stickyTop) {
+      utils.removeClass(sticky, 'is-sticky');
+    }
+  }
+
+  handleScroll() {
+    const sticky = document.querySelector('[data-toggle="sticky-onscroll"]');
+    this.stickyToggle(sticky);
+  }
+
+  render() {
+    return (
+      <nav id="top-nav" className="navbar navbar-full navbar-custom navbar-fixed-top" data-toggle="sticky-onscroll">
+        <div className="container">
+          <div className="navbar-header">
+            <button className="navbar-toggler hidden-md-up pull-xs-right pull-sm-right" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+              &#9776;
+            </button>
+            <Link to="#page-top" className="navbar-brand page-scroll"><img src={logo}/></Link>
+          </div>
+          <div className="collapse navbar-toggleable-sm" id="collapsingNavbar">
+            <ul className="nav navbar-nav pull-md-right">
+              <li className="nav-item">
+                <Link to="#reason" className="page-scroll nav-link">Why Us</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="#team" className="page-scroll nav-link">Who We Are</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="#services" className="page-scroll nav-link">What We Do</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="#sponsor" className="page-scroll nav-link">Sponsors</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="collapse navbar-toggleable-sm" id="collapsingNavbar">
-          <ul className="nav navbar-nav pull-md-right">
-            <li className="nav-item">
-              <Link to="#team" className="page-scroll nav-link">Who We Are</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="#services" className="page-scroll nav-link">What We Do</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    );
+  }
 }
 
 export default Header;
--- src/js/components/home/HomePage.js
+++ src/js/components/home/HomePage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 
 import introImage from '../../../img/intro.png';
 import people from '../../../img/people.png';
+import girl from '../../../img/girl_on_computer.jpg';
 import puzzle1 from '../../../img/puzzle1.png';
 import puzzle2 from '../../../img/puzzle2.png';
 import puzzle3 from '../../../img/puzzle3.png';
@@ -21,18 +22,12 @@ class HomePage extends React.Component {
             <div className="container">
               <div className="row">
                 <div className="intro-text">
-                  <div className="col-md-6 col-xs-12 pull-md-right pull-xs-none">
-                    <div id="banner">
-                      <div id="underlay-image"><img src={introImage}/></div>
-                      <div><img src={introImage} alt="background image" /></div>
-                    </div>
-                  </div>
                   <div className="col-md-6 col-xs-12">
-                    <div className="home-content">
+                    <div className="home-content body-pad">
                       <h1>We Teach Teens to Code</h1>
                       <p>Professional software developers taking time outside of 
                         work to teach teenagers in high school how to code... for free!</p>
-                      <p><span className="action">Apply</span></p>
+                      <p><Link to="#" className="action">Apply</Link></p>
                     </div>
                   </div>
               </div>
@@ -42,49 +37,47 @@ class HomePage extends React.Component {
       </div>
 
       <section id="reason" className="why">
+        <div className="container">
           <div className="row">
-              <div className="col-lg-10 offset-lg-1 why-bg">
-                  <div className="col-lg-4 why-section">
-                    <img src={people} alt="Why" />
+            <div className="col-lg-12 why-bg">
+                <div className="col-lg-4 why-section">
+                  <img src={girl} alt="Why" />
+                </div>
+                <div className="col-lg-2 why-section last">
+                </div>
+                <div className="col-lg-6 why-section last">
+                  <div className="why-text body-pad">
+                      <h4>Why?</h4>
+                      <div>
+                          <p className="text-muted">Everyone in this country should learn how to program a computer.
+                            It teaches you to think. - Steve Jobs</p>
+                            <p className="text-muted">It's more about the proces of breaking down problems into simpler units
+                            than coming out with complex algorithms. - Makinde, early Facebook Engineer</p>
+                      </div>
                   </div>
-                  <div className="col-lg-2 why-section last">
-                  </div>
-                  <div className="col-lg-6 why-section last">
-                    <div className="col-lg-8 offset-lg-2 why-text">
-                        <h4>Why?</h4>
-                        <div>
-                            <p className="text-muted">Everyone in this country should learn how to program a computer.
-                              It teaches you to think. - Steve Jobs</p>
-                              <p className="text-muted">It's more about the proces of breaking down problems into simpler units
-                              than coming out with complex algorithms. - Makinde, early Facebook Engineer</p>
-                        </div>
-                    </div>
-                  </div>
-              </div>
+                </div>
+            </div>
           </div>
+        </div>
       </section>
 
       <section id="team" className="whoami whoami-bg">
         <div className="container">
           <div className="row">
-              <div className="col-lg-10 offset-lg-1">
-                  <div className="col-lg-4 whoami-section">
-                    <img src={people} alt="Who we are" />
-                  </div>
-                  <div className="col-lg-2 whoami-section last">
-                  </div>
-                  <div className="col-lg-6 whoami-section last">
-                    <div className="col-lg-8 offset-lg-2 whoami-text">
-                        <h4>Who We Are</h4>
-                        <div>
-                            <p className="text-muted">We are Andelans. 
-                              An entire community of software devlopers in Lagos and Nairobi. 
-                              We work with tech companies around the world.
-                            </p>
-                        </div>
-                    </div>
+            <div className="col-lg-6 whoami-section">
+              <div className="whoami-text body-pad">
+                  <h4>Who We Are</h4>
+                  <div>
+                      <p className="text-muted">We are Andelans. 
+                        An entire community of software devlopers in Lagos and Nairobi. 
+                        We work with tech companies around the world.
+                      </p>
                   </div>
               </div>
+            </div>
+            <div className="col-lg-6 whoami-section">
+              <img src={people} alt="Who we are" />
+            </div>
           </div>
         </div>
       </section>
@@ -92,13 +85,13 @@ class HomePage extends React.Component {
       <section id="services" className="bg-light-blue">
           <div className="container">
               <div className="row">
-                  <div className="col-lg-12 text-md-center">
+                  <div className="col-lg-12 text-xs-center body-pad">
                       <h4 className="section-heading">How we do this?</h4>
                       <h3 className="section-subheading text-muted"></h3>
                   </div>
               </div>
               <div className="row">
-                  <div className="col-lg-10 offset-lg-1 col-xs-12">
+                  <div className="col-lg-10 offset-lg-1 col-xs-12 body-pad">
                     <div className="col-md-6 service-col">
                         <div className="col-md-2 col-sm-2 pull-sm-left pull-xs-right">
                           <img className="puzzle" src={puzzle1} alt="puzzle" />
@@ -143,9 +136,11 @@ class HomePage extends React.Component {
       <section id="video" className="why">
           <div className="container">
               <div className="row">
-                  <div className="col-lg-12 text-md-center">
-                      <div className="embed-responsive embed-responsive-16by9">
-                          <iframe width="560" height="315" src="https://www.youtube.com/embed/Fkd9TWUtFm0" allowFullScreen></iframe>
+                  <div className="col-lg-12 text-md-center body-pad embed-pad">
+                      <div className="embed">
+                        <div className="embed-responsive embed-responsive-16by9">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/Fkd9TWUtFm0" allowFullScreen></iframe>
+                        </div>
                       </div>
                       <p className="text-muted lead">
                         Thomas Suarez - 12 year old app developer 
@@ -159,7 +154,7 @@ class HomePage extends React.Component {
       <section id="sponsor" className="bg-light-blue">
           <div className="container">
               <div className="row">
-                  <div className="col-lg-12 text-md-center">
+                  <div className="col-lg-12 text-md-center body-pad">
                       <h4 className="section-subheading text-muted">#teencode is proudly supported by </h4>
                       <h1 className="section-heading text-muted">&lt; Andela &gt;</h1>
                   </div>
