1318e9331c0d8c7ab6dc3c12b0413e23e889c308
Innocent <innocent.amadi@andela.com>
2017-04-01 10:49:43 +0100

    comment out duplicated segment snippet

--- src/js/components/home/HomePage.js
+++ src/js/components/home/HomePage.js
@@ -8,10 +8,7 @@ class HomePage extends React.Component {
   }
 
   componentDidMount() {
-    // debugger;
-    // let analytics = window.analytics || []
-    // if (analytics.initialize) return
-    // window.analytics.page();
+    window.analytics.page();
   }
 
   viewForm(e) {
@@ -207,7 +204,7 @@ class HomePage extends React.Component {
           <div className="container">
               <div className="row">
                   <div className="col-md-12 text-md-center body-pad">
-                      <h3>#teencode is proudly powered by</h3>
+                      <h3>#teencode is powered by</h3>
                   </div>
                   <div className="col-md-4 offset-md-4 col-xs-12">
                     <a href="http://andela.com" target="_blank"><img className="andela-logo" src={require('../../../img/andela-white-logo.png')} alt="Andela" /></a>
