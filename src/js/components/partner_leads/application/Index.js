d52bcb31c9dc79d74a094906d609a3ad6b57bea7
Innocent <innocent.amadi@andela.com>
2017-04-01 18:54:18 +0100

    modify eligibility page

--- src/js/components/partner_leads/application/Index.js
+++ src/js/components/partner_leads/application/Index.js
@@ -4,49 +4,49 @@ import logo from '../../../../img/logo.png';
 
 const ApplicationPage = () => {
   return (
-    <div>
-      <div className="modal-overlay">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 recipient">
-              <div className="to">To:</div>
-              <div className="name">
-                <p>TeenCodeAfrica</p>
-                <p>teencodeafrica@gmail.com</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="container">
-              <p>Hi</p>
-              <div className="form">
-                <form>
-                  <div className="form-group row">
-                    <label for="example-text-input" className="col-2 col-form-label">Text</label>
-                    <div className="col-10">
-                      <input className="form-control form-control-lg" type="text" value="Artisanal kale" id="example-text-input" />
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label for="example-search-input" className="col-2 col-form-label">Search</label>
-                    <div className="col-10">
-                      <input className="form-control" type="search" value="How do I shoot web" id="example-search-input" />
-                    </div>
-                  </div>
-                  <div className="form-group row">
-                    <label for="example-email-input" className="col-2 col-form-label">Email</label>
-                    <div className="col-10">
-                      <input className="form-control" type="email" value="bootstrap@example.com" id="example-email-input" />
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="container eligibility-check">
+      <div className="header">
+        <img src={logo} />
+        <Link to="/">
+          <i className="fa fa-window-close" aria-hidden="true" />
+        </Link>
       </div>
+
+      <div className="page-title">
+        <h2>You are eligible!!!</h2>
+        <small>Join us in building Africa's next generation of Tech Leaders!</small>
+      </div>
+
+      <content className="wrapper">
+
+        <div className="container">
+          <form>
+            <div className="form-group row">
+              <label for="inputEmail3" className="col-md-4 col-sm-12 col-form-label">Name of your school:</label>
+              <div className="col-md-8 col-sm-12">
+                <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
+              </div>
+            </div>
+            <div className="form-group row">
+              <label for="inputPassword3" className="col-md-4 col-sm-12  col-form-label">Your email:</label>
+              <div className="col-md-8 col-sm-12">
+                <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
+              </div>
+            </div>
+            <div className="form-group row">
+              <label for="inputPassword3" className="col-md-4 col-sm-12  col-form-label">Your phone number:</label>
+              <div className="col-md-8 col-sm-12">
+                <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
+              </div>
+            </div>
+            <button className="btn btn-action" >Apply</button>
+          </form>
+        </div>
+
+
+      </content>
     </div>
+
   )
 }
 
