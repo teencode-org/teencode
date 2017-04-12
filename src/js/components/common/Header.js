0f934cd92e8fc89e5e8e33abef3e4d4c99a5dbf2
Chineze Nwosu <chineze.nwosu@andela.com>
2017-04-12 19:39:22 +0100

    remove unnecessary jquery, abstract navLink into component

--- src/js/components/common/Header.js
+++ src/js/components/common/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
+import NavLink from './NavLink';
+import utils from '../../utils/helpers'
+import jquery from 'jquery';
 
 class Header extends React.Component {
   constructor(props) {
@@ -17,10 +20,9 @@ class Header extends React.Component {
   }
 
   navLinkOnClick(event) {
-    let target = $(event.target).attr('href') ? event.target : $(event.target).parents(".page-scroll")[0];
-    let href = $(target).attr('href');
-    $('html, body').stop().animate({
-        scrollTop: ($(href).offset().top - 50)
+    let href = event.currentTarget.getAttribute('href');
+    jquery('html, body').stop().animate({
+        scrollTop: (jquery(href).offset().top - 50)
     }, 1250);
     event.preventDefault();
   }
@@ -29,46 +31,24 @@ class Header extends React.Component {
     return window.location.pathname === '/';
   }
 
-  generateDropdownLink(path, name, linkMap) {
-    let links = Object.keys(linkMap).map((key, index) => {
-      return (
-        <Link key={index} to={`#${key}`} className="page-scroll dropdown-item" onClick={this.navLinkOnClick}>{linkMap[key]}</Link>
-      )
-    });
-    if (window.location.pathname === path) {
-      return (
-        <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" onlyActiveOnIndex activeClassName="active" to="#" data-href={path} id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            {name} <span className="caret down"></span>
-          </Link>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            {links}
-          </div>
-        </li>
-      )
-    }
-    return (
-      <li className="nav-item">
-        <Link to={path} activeClassName="active" onlyActiveOnIndex className="nav-link">{name}</Link>
-      </li>
-    )
-  }
-
-  handleScroll() {
-    const sticky = document.querySelector('[data-toggle="sticky-onscroll"]');
+  handleScroll(event) {
+    const sticky = event.target.querySelector('[data-toggle="sticky-onscroll"]');
     const stickyHeight = sticky.clientHeight;
     const stickyTop = sticky.offsetTop;
     const scrollTop = document.body.scrollTop;
 
     if (scrollTop > stickyHeight + stickyTop){
-      $(sticky).addClass('is-sticky');
+      utils.addClass(sticky, 'is-sticky');
     } else if (scrollTop <= stickyTop) {
-      this.isHomeLink() && $(sticky).removeClass('is-sticky');
+      if (this.isHomeLink()) {
+        utils.removeClass(sticky, 'is-sticky');
+      }
     }
+
   }
 
   render() {
-    let stickyClass = this.isHomeLink() ? '' : 'is-sticky'
+    let stickyClass = this.isHomeLink() ? '' : 'is-sticky';
     return (
       <nav id="top-nav" className={`navbar navbar-full navbar-custom navbar-fixed-top ${stickyClass}`} data-toggle="sticky-onscroll">
         <div className="container">
@@ -80,13 +60,18 @@ class Header extends React.Component {
           </div>
           <div className="collapse navbar-toggleable-sm" id="collapsingNavbar">
             <ul className="nav navbar-nav pull-md-right">
-              {this.generateDropdownLink('/', 'Home', {how: 'Process', testimonials: 'Testimonials', sponsor: 'Sponsors'})}
-              <li className="nav-item">
-                <Link to="/volunteer" activeClassName="active" className="nav-link">Volunteers</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/curriculum" activeClassName="active" className="nav-link">Curriculum</Link>
-              </li>
+              <NavLink
+                path="/"
+                name="Home"
+                navLinkOnClick={this.navLinkOnClick}
+                subLinks={{
+                  how: 'Process',
+                  testimonials: 'Testimonials',
+                  sponsor: 'Sponsors'
+                }}
+              />
+              <NavLink path="/volunteer" name="Volunteers" />
+              <NavLink path="/curriculum" name="Curriculum" />
             </ul>
           </div>
         </div>
---
+++ src/js/components/common/NavLink.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Link } from 'react-router';
+import jquery from 'jquery';
+
+class NavLink extends React.Component {
+  generateNavLink() {
+    let links = Object.keys(this.props.subLinks).map((key) => {
+      return (
+        <Link
+          key={key}
+          to={`#${key}`}
+          className="page-scroll dropdown-item"
+          onClick={this.props.navLinkOnClick}
+        >
+          {this.props.subLinks[key]}
+        </Link>
+      )
+    });
+
+    if (window.location.pathname === this.props.path && Object.keys(this.props.subLinks).length) {
+      return (
+        <li className="nav-item dropdown">
+          <Link
+            className="nav-link dropdown-toggle"
+            onlyActiveOnIndex
+            activeClassName="active"
+            to="#"
+            data-href={this.props.path}
+            id="navbarDropdownMenuLink"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            {this.props.name} <span className="caret down"></span>
+          </Link>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            {links}
+          </div>
+        </li>
+      )
+    }
+
+    return (
+      <li className="nav-item">
+        <Link
+          to={this.props.path}
+          activeClassName="active"
+          onlyActiveOnIndex
+          className="nav-link"
+        >
+          {this.props.name}
+        </Link>
+      </li>
+    )
+  }
+
+  render() {
+    return this.generateNavLink();
+  }
+}
+
+NavLink.defaultProps = {
+  subLinks: {}
+}
+
+NavLink.propTypes = {
+  path: React.PropTypes.string.isRequired,
+  name: React.PropTypes.string.isRequired,
+  navLinkOnClick: React.PropTypes.func,
+  subLinks: React.PropTypes.object
+}
+
+export default NavLink;
