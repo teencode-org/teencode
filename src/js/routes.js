23d8502406538ac3bfd54e8fdaa8b2d76db1d21d
Chineze Nwosu <chineze.nwosu@andela.com>
2017-04-12 11:37:59 +0100

    create route for curriculum

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
+import CurriculumPage from './components/curriculum/CurriculumPage';
 import ApplicationPage from './components/partner_leads/application/Index';
 import AppreciationPage from './components/partner_leads/appreciation/Index';
 import EligibilityCheck from './components/partner_leads/eligibility_check/Index';
@@ -11,6 +12,7 @@ export default (
   <Route>
     <Route path="/" component={App} >
       <IndexRoute component={HomePage} />
+      <Route path="curriculum" component={CurriculumPage} />
     </Route>
 
     <Route path="/partner-leads" component={PartnerLeadsPage} >
