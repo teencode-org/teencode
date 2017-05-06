51a7465e8b0d50cb460b4a07700e13fadd7b667d
Tijesunimi Peters <tijesunimi.peters@andela.com>
2017-05-06 08:20:41 +0100

    partner leads testing finished

---
+++ src/tests/partner_leads/eligibility_check/Index.spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Index from '../../../js/components/partner_leads/eligibility_check/Index';
+
+describe('Eligibility Check Index', () => {
+  const IndexWrapper = shallow(<Index />);
+
+  it('renders the accurate Component', () => {
+
+    const smallElement = IndexWrapper.find('.page-title');
+    expect(IndexWrapper.find('.container.eligibility-check')).toExist();
+    expect(smallElement.childAt(1).is('small')).toBeTruthy();
+    expect(smallElement.childAt(1).text()).toEqual('For Schools');
+    expect(smallElement.childAt(0).is('h2')).toBeTruthy();
+    expect(smallElement.childAt(0).text()).toEqual('Eligibility Check');
+    expect(IndexWrapper.find('SingleQuestion').length).toEqual(3);
+  });
+  
+});
