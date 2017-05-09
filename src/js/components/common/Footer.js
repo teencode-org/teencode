cd7e4296dd63562a8befcb7a3cc4805b2a0f964a
Innocent <innocent.amadi@andela.com>
2017-05-09 10:48:05 +0100

    make footer responsive

--- src/js/components/common/Footer.js
+++ src/js/components/common/Footer.js
@@ -46,7 +46,7 @@ class Footer extends React.Component {
       <footer>
         <div className="container">   
           <div className="row footer-top">
-            <div className="col-sm-3 col-xs-10 eligibility">
+            <div className="col-md-3 col-sm-10 col-xs-10 eligibility">
               <p>
                 Are you a High School administrator or computer teacher and want to be a part of this?
               </p>
@@ -54,13 +54,13 @@ class Footer extends React.Component {
                 <i className="fa fa-question-circle"></i>Check if your school is eligible
               </Link>
             </div>
-            <div className="offset-sm-1 col-sm-4 col-xs-10 vision">
+            <div className="offset-md-1 col-md-4 col-sm-10 col-xs-10 vision">
               <p>
                 Our vision is the widespread adoption of Computer Programming in High Schools in Africa with a syllabus maintained by experts in the field.
               </p>
               <Link className="more" to="/#how" onClick={this.clickLink}>Learn more about our mission and vision >> </Link>
             </div>
-            <div className="offset-sm-1 col-sm-3 col-xs-10 feedback">
+            <div className="offset-md-1 col-md-3 col-sm-10 col-xs-10 feedback">
               <p>
                 CONTACT US <br/>
                 <i className="fa fa-envelope-o"></i>
@@ -76,22 +76,22 @@ class Footer extends React.Component {
         <div className="footer-bottom">
           <div className="container">
             <div className="row">
-              <div className="col-sm-3 col-xs-10">
+              <div className="col-md-3 col-sm-12 col-xs-12">
                 <span className="copyright">
                   (c) 2017 TEENCODE AFRICA
                 </span>
               </div>
-              <div className="offset-sm-2 col-sm-2 col-xs-2 socials">
+              <div className="offset-md-2 col-md-2 col-sm-12 col-xs-12 socials">
                 <a href="https://www.facebook.com/Teencode-Africa-1345535292156762/" target="_blank">
                   <i className="fa fa-facebook-square fa-2x"></i>
                 </a>
                 
                 <a href="#" target="_blank"><i className="fa fa-twitter-square fa-2x"></i></a>
               </div>
-              <div className="offset-sm-2 col-sm-2 col-xs-6 policy">
+              <div className="offset-md-2 col-md-2 col-sm-5 col-xs-6 policy">
                 <a href="#">PRIVACY POLICY</a>
               </div>
-              <div className="col-sm-1 col-xs-6 sitemap">
+              <div className="col-md-1 col-sm-5 col-xs-6 sitemap">
                 <a href="#">SITEMAP</a>
               </div>
             </div>
