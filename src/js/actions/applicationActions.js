9a00940f8be48ed2b627a8bff1debcc266533901
Chineze Nwosu <chineze.nwosu@andela.com>
2017-05-15 14:15:18 +0100

    abstract common action functions

--- src/js/actions/applicationActions.js
+++ src/js/actions/applicationActions.js
@@ -1,54 +1,19 @@
-import actionTypes from "../constants/actionTypes"
-import webAPI from '../utils/webAPI'
-import validator from '../utils/validator'
-import { receiveError } from './errorActions'
-
-export const applyActions = () => {
-  return {
-    request: (data) => {
-      return {
-        type: actionTypes.REQUEST_SEND_APPLICATION,
-        payload: { data }
-      }
-    },
-    receive: (data) => {
-      return {
-        type: actionTypes.RECEIVE_SEND_APPLICATION,
-        payload: { receivedAt: new Date() }
-      }
-    },
-    fail: (errors) => {
-      return {
-        type: actionTypes.FAIL_SEND_APPLICATION,
-        payload: { receivedAt: new Date() }
-      }
-    }
-  }
-}
+import actionTypes from '../constants/actionTypes'
+import { handleApiCall, baseActions } from './helpers';
 
 export const apply = (data) => {
-  let callApplyActions = applyActions()
-  return function(dispatch) {
-    const {isValid, concatenatedErrors} = validator.validate(data);
-    
-    dispatch(callApplyActions.request(data))
-    if (isValid) {
-    return webAPI('/applicants', 'POST', data)
-      .then(response => {
-        if (response.errors) {
-          dispatch(callApplyActions.fail(response.errors))
-          dispatch(receiveError('An error occurred. Please try again later.', 'application'))
-        } else {
-          dispatch(callApplyActions.receive(response))
-        }
-      })
-      .catch(errors => {
-        dispatch(callApplyActions.fail(errors))
-        dispatch(receiveError('An error occurred. Please try again later.', 'application'))
-      });
-    } else {
-      dispatch(callApplyActions.fail(concatenatedErrors))
-      dispatch(receiveError(concatenatedErrors, 'application'))
-    }
-  }
+  let actions = baseActions({
+    requestType: actionTypes.RECEIVE_SEND_APPLICATION,
+    receiveType: actionTypes.FAIL_SEND_APPLICATION,
+    failType: actionTypes.FAIL_SEND_APPLICATION
+  });
+
+  return handleApiCall({
+    actions,
+    data,
+    errorMessage: 'An error occurred. Please try again later.',
+    caller: 'application',
+    route: '/applicants',
+    requestMethod: 'POST'
+  });
 }
--- src/js/actions/contactActions.js
+++ src/js/actions/contactActions.js
@@ -1,55 +1,20 @@
-import actionTypes from "../constants/actionTypes"
-import webAPI from '../utils/webAPI'
-import validator from '../utils/validator';
-import { receiveError } from './errorActions'
-
-export const sendContactActions = () => {
-  return {
-    request: (data) => {
-      return {
-        type: actionTypes.REQUEST_SEND_FEEDBACK,
-        payload: { data }
-      }
-    },
-    receive: (data) => {
-      return {
-        type: actionTypes.RECEIVE_SEND_FEEDBACK,
-        payload: { receivedAt: new Date() }
-      }
-    },
-    fail: (errors) => {
-      return {
-        type: actionTypes.FAIL_SEND_FEEDBACK,
-        payload: { errors, receivedAt: new Date() }
-      }
-    }
-  }
-}
+import actionTypes from '../constants/actionTypes';
+import { handleApiCall, baseActions } from './helpers';
 
 export const sendFeedback = (data) => {
-  let callSendContactActions = sendContactActions()
-  return function(dispatch) {
-    const {isValid, concatenatedErrors} = validator.validate(data);
-
-    dispatch(callSendContactActions.request(data))
-    if (isValid) {
-      return webAPI('/inquiries', 'POST', data)
-        .then(response => {
-          if (response.errors) {
-            dispatch(callSendContactActions.fail(response.errors))
-            dispatch(receiveError('Something prevented your message from submitting successfully. Please try again later.', 'contact'))
-          } else {
-            dispatch(callSendContactActions.receive(response))
-          }
-        })
-        .catch(errors => {
-          dispatch(callSendContactActions.fail(errors))
-          dispatch(receiveError('Something prevented your message from submitting successfully. Please try again later.', 'contact'))
-        });
-    } else {
-      dispatch(callSendContactActions.fail(concatenatedErrors))
-      dispatch(receiveError(concatenatedErrors, 'contact'))
-    }
-  }
+  let actions = baseActions({
+    requestType: actionTypes.REQUEST_SEND_FEEDBACK,
+    receiveType: actionTypes.RECEIVE_SEND_FEEDBACK,
+    failType: actionTypes.FAIL_SEND_FEEDBACK
+  });
+  
+  return handleApiCall({
+    actions,
+    data,
+    errorMessage: 'Something prevented your message from submitting successfully. Please try again later.',
+    caller: 'contact',
+    route: '/inquiries',
+    requestMethod: 'POST'
+  });
 }
 
