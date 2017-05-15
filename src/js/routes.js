c00b9f2d97b646a645c5211207c2a4bf54029ea3
Morolake Orekoya <morolake.orekoya@andela.com>
2017-05-16 00:34:58 +0100

    add about-us route

--- src/js/routes.js
+++ src/js/routes.js
@@ -15,6 +15,7 @@ import Feedback from './components/feedback/Index';
 import ContactUsPage from './components/contact_us/Index';
 import NotFound from './components/not_found/notFound';
 import Blog from './components/blog';
+import AboutUsPage from './components/about/AboutUsPage';
 
 const redirectIfFlagIsDisabled = (flag, nextState, replace) => {
   if (flag) return;
@@ -37,9 +38,13 @@ export default (
              onEnter={redirectIfFlagIsDisabled.bind(null, flagChecks.contactUsIsEnabled())} />
       <Route path="/blog" component={Blog} />
       <Route path="/not-found" component={NotFound} />
+      <Route path="about-us"
+             component={AboutUsPage}
+             onEnter={redirectIfFlagIsDisabled.bind(null, flagChecks.aboutUsIsEnabled())} />
     </Route>
+    <Route path="/not-found" component={NotFound} />
 
-    
+
     // TODO: Create template for feedback and pass in custom messages
     <Route path="/feedback" component={Feedback} >
       <Route path="success/application" component={ApplicationSuccessPage} />
