ed8a3f330dabcfcea50945eb8a5ecb16cf131302
Morolake Orekoya <morolake.orekoya@andela.com>
2017-05-16 00:35:45 +0100

    add flags for about us page

--- tools/featureFlags.js
+++ tools/featureFlags.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 export default {
   'CURRICULUM_ENABLED': process.env.CURRICULUM_ENABLED,
+  'ABOUT_US_ENABLED': process.env.ABOUT_US_ENABLED,
   'CONTACT_US_ENABLED': process.env.CONTACT_US_ENABLED,
   'VOLUNTEER_PAGE_ENABLED': process.env.VOLUNTEER_PAGE_ENABLED,
   'MISSION_PAGE_ENABLED': process.env.MISSION_PAGE_ENABLED,
