95d7173a7f65f4247f0e3867dc0416a3facacdf3
Chineze Nwosu <chineze.nwosu@andela.com>
2017-05-19 12:07:25 +0100

    abstract middleware logic to function

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -3,20 +3,25 @@ import rootReducer from '../reducers/index';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger'
 
-export default function configureStore(initialState) {
-  let logger = createLogger()
-  let middleware = applyMiddleware(thunk)
-
-  if (process.ENV !== 'production') {
-    middleware = applyMiddleware(thunk, logger)
-  }
-
+const configureStore = (initialState) => {
   return createStore(
     rootReducer,
     compose(
-      middleware,
+      getMiddleware(),
       window.devToolsExtension? window.devToolsExtension() : (empty) => empty
     )
   );
 }
+
+const getMiddleware = () => {
+  let logger = createLogger();
+  let middleware = applyMiddleware(thunk);
+
+  if (process.ENV !== 'production') {
+    middleware = applyMiddleware(thunk, logger);
+  }
+  return middleware;
+}
+
+export default configureStore;
 
