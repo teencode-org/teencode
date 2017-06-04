c677c9f8f4431300174737cd9e4e6facc3c5a665
Tijesunimi Peters <tijesunimi.peters@andela.com>
2017-06-05 00:08:42 +0100

    mobile view finished

--- src/js/components/blog/Blog.js
+++ src/js/components/blog/Blog.js
@@ -7,13 +7,13 @@ export default class Blog extends Component {
     return (
       <div>
         <div className="row blog-header">
-          <div className="col-xs-12 col-md-7 left-col">
+          <div className="col-sm-12 col-md-7 left-col">
             <div className="description">
               <h1>Brilliance is evenly distributed.<br/>We're doing the same for opportunity</h1>
               <p>By Rowland Henshaw</p>
             </div>
           </div>
-          <div className="col-xs-12 col-md-5 right-col">
+          <div className="col-sm-12 col-md-5 right-col">
             <div className="row top-row">
               <div className="description">
                 <h3>Teencode facilitators taken on<br /> Koriobangi</h3>
@@ -30,8 +30,10 @@ export default class Blog extends Component {
           <div className="blog-list">
             <h3>Top articles</h3>
             <div className="row blog-article">
-              <div className="col-md-3 col-xs-12 blog-article-image"></div>
-              <div className="col-md-9 col-xs-12 blog-article-info">
+              <div className="blog-article-image">
+                <img src="http://via.placeholder.com/300x300" />
+              </div>
+              <div className="blog-article-info">
                 <h2 className="article-title">There will be no prisons if our minds were free</h2>
                 <p className="article-author">by Rowland Henshaw</p>
                 <p className="article-summary">
@@ -41,8 +43,10 @@ export default class Blog extends Component {
               </div>
             </div>
             <div className="row blog-article">
-              <div className="col-xs-12 col-md-3 blog-article-image"></div>
-              <div className="col-xs-12 col-md-9 blog-article-info">
+              <div className="blog-article-image">
+                <img src="http://via.placeholder.com/300x300" />
+              </div>
+              <div className="blog-article-info">
                 <h2 className="article-title">Gender balance in tech could be achieved if only we start early.</h2>
                 <p className="article-author">by Rowland Henshaw</p>
                 <p className="article-summary">
