9478613bc3ca756615373b443d74b47645fa8283
Innocent <innocent.amadi@andela.com>
2017-07-08 20:57:38 +0100

    add facilitator leads, correct name spellings

--- src/js/components/about/data.js
+++ src/js/components/about/data.js
@@ -6,12 +6,14 @@ export const PartnersImages = [
 
 export const Leads = [
   {name: 'Innocent Amadi', position: 'GENERAL LEAD'},
-  {name: 'Kehinde Ogunde', position: 'LEAD, CURRICULUM'},
+  {name: 'Kehinde Oni', position: 'LEAD, CURRICULUM'},
   {name: 'Gertrude Nyenyeshi', position: 'LEAD, SCOUTS NAIROBI'},
   {name: 'Bisoye Atalagbe', position: 'LEAD, SCOUTS LAGOS'},
   {name: 'Ogbara Godwin', position: 'LEAD, ENGINNEERING'},
   {name: 'Winner-T. Bolorunduro', position: 'LEAD, PROJECT ADMINISTRATION'},
-  {name: 'Tolupe Komolafe', position: 'LEAD, FINANCE/LOGISTICS LAGOS'},
+  {name: 'Olaide Agboola', position: 'LEAD, FINANCE/LOGISTICS LAGOS'},
   {name: 'Loice Andia', position: 'LEAD, FINANCE/LOGISTICS NAIROBI'},
+  {name: 'Moyosore Sosan', position: 'LEAD, FACILITATORS LAGOS'},
+  {name: 'Tonida Baraza', position: 'LEAD, FACILITATORS NAIROBI'},
   {name: 'Austin Roy', position: 'LEAD, COMMUNICATION'}
 ]
