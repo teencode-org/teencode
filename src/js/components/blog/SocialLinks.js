70380a5f674b01bb5ca399f7ca2e7271e7db9ea0
Morolake Orekoya <morolake.orekoya@andela.com>
2017-07-13 14:32:31 +0100

    add static blog page

---
+++ src/js/components/blog/SocialLinks.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SocialLinks = () => {
+  return (
+    <div className="blog-socials">
+      <a href="https://www.facebook.com/groups/teencodeafrica/" target="_blank">
+        <i className="fa fa-facebook-square fa-2x"></i>
+      </a>
+      <a href="https://twitter.com/TeencodeA" target="_blank">
+        <i className="fa fa-twitter-square fa-2x"></i>
+      </a>
+      <a href="#" target="_blank">
+        <i className="fa fa-linkedin-square fa-2x"></i>
+      </a>
+    </div>
+  );
+}
+
+export default SocialLinks;
