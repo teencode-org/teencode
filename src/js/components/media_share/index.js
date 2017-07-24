124c1fc7e4de9cb8acf498b3615555b253ed78f1
Chukwuma Ezumezu <chukwuma.ezumezu@andel.com>
2017-07-24 17:19:04 +0100

    change url

--- src/js/components/media_share/index.js
+++ src/js/components/media_share/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { trimText } from '../../utils/helpers';
 
 const MediaShare = () => {
-  // const url = window.location.href;
+  const url = window.location.href;
   const handle = '@TeencodeA';
   const longText = 'Data structures are a critical part of software development, and one of the most common topics for developer job interview questions. The good news is that they’re basically just specialized formats for organizing and storing data. Im going to teach you 10 of the most common data structures — right here in this short article. Ive embedded videos that I created for each of these data structures. I’ve also linked to code examples for each of them, which show how to implement these in JavaScript.';
-  const url = 'https://koshin.herokuapp.com/';
   const tweetLength = 140 - (url.length + handle.length + 2)
   return (
     <div className="share-view">
