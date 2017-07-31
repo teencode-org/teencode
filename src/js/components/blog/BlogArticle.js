1070ab3e2cc1571a77d9968a88d71b0baa776581
Chineze <andeladeveloper@Andelas-MacBook-Pro.local>
2017-07-31 14:48:21 +0100

    fix styling for blog page

--- src/js/components/blog/BlogArticle.js
+++ src/js/components/blog/BlogArticle.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import SocialLinks from './SocialLinks';
-import {  Article } from './mockData';
+import { Article } from './mockData';
 
 class BlogArticle extends React.Component {
   componentDidMount() {
@@ -15,31 +15,49 @@ class BlogArticle extends React.Component {
 
   render() {
     return (
-    <div className="row blog-full-article">
-      <h2 className="article-title">{Article.title}</h2>
-      <div className="blog-article-author">
-        <img src={Article.author.imageUrl} />
-        <p className="article-author">{Article.author.name}</p>
-        <p className="article-tagline">{Article.author.tagline}</p>
-      </div>
-      <img className="article-image" src={Article.imageUrl} />
-      <SocialLinks />
-      <div className="article-body" ref={this.setArticleBody} />
-      <SocialLinks />
-      <div className="suggested-reading">
-        <h2>Suggested Reads</h2>
-        {Article.suggested.map((article, index) => (
-          <div
-            key={`suggested-${index}`} 
-            style={{ background: `url(${article.imageUrl}) no-repeat` }}
-            className="col-md-5"
-          >
-            <h4>{article.title}</h4>
-            <p>by {article.author}</p>
+      <div className="blog-full-article">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h2 className="article-title">{Article.title}</h2>
+              <div className="blog-article-author">
+                <img src={Article.author.imageUrl} />
+                <p className="article-author">{Article.author.name}</p>
+                <p className="article-tagline">{Article.author.tagline}</p>
+              </div>
+            </div>
           </div>
-        ))}
+        </div>
+        <img className="article-image" src={Article.imageUrl} />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <SocialLinks />
+              <div className="article-body" ref={this.setArticleBody} />
+              <SocialLinks />
+              <div className="suggested-reading row">
+                <div className="col-md-12">
+                  <h2 className="suggested-reading-title">Suggested Reads</h2>
+                </div>
+                {Article.suggested.map((article, index) => (
+                  <div
+                    key={`suggested-${index}`} 
+                    className="col-md-6 col-sm-12"
+                  >
+                    <div
+                      className="suggested-reading-link"
+                      style={{ background: `url(${article.imageUrl}) no-repeat` }}
+                    >
+                      <h4 className="suggested-reading-name">{article.title}</h4>
+                      <p>by {article.author}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   );
   }
 }
