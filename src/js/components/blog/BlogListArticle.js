e010deb4696a48cda6bdf8d1bd98100de378b4b2
Innocent <innocent.amadi@andela.com>
2017-08-07 16:14:29 +0100

    correct article rendering in home page

--- src/js/components/blog/BlogListArticle.js
+++ src/js/components/blog/BlogListArticle.js
@@ -1,19 +1,20 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { parseBlogTitle } from '../../utils/parseBlogTitle';
+import { stripHtmlTags } from '../../utils/helpers';
 
 const BlogListArticle = ({ id, imageUrl, title, author, summary }) => {
   return (
     <Link to={`/blog/${id}/${parseBlogTitle(title)}`}>
     <div className="row blog-article">
       <div className="blog-article-image">
-        <img src={imageUrl} />
+        <img width="300" height="300" src={imageUrl} />
       </div>
       <div className="blog-article-info">
         <h2 className="article-title">{title}</h2>
         <p className="article-author">{author}</p>
         <p className="article-summary">
-          {summary}
+          {stripHtmlTags(summary)}
         </p>
         <button className="btn">Read More ></button>
       </div>
