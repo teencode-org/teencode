6794dc5d7d0a8e42bbe0edc39fc954f26b81e365
Morolake Orekoya <morolake.orekoya@andela.com>
2017-09-07 13:25:10 +0100

    add list of faciltator guide and lesson notes links

---
+++ src/js/components/curriculum/facilitatorGuideList.js
@@ -0,0 +1,39 @@
+const facilitatorGuideList = [  
+  {  
+    id:4,
+    facilitatorGuideUrl: 'https://docs.google.com/document/d/1sk_zXANIGQ4RtcUg8tPDIN8DRTGYRI0BVFD7yiJarhY',
+    lessonNotesUrl:'https://docs.google.com/document/d/1sk_zXANIGQ4RtcUg8tPDIN8DRTGYRI0BVFD7yiJarhY'
+  },
+  {  
+    id:3,
+    facilitatorGuideUrl: '',
+    lessonNotesUrl:''
+  },
+  {  
+    id:7,
+    facilitatorGuideUrl: 'https://docs.google.com/document/d/1mP3mHjefLciAW-5X__rbAim7rxQ__sYQMWF_7tBf36Q/edit',
+    lessonNotesUrl:'https://docs.google.com/document/d/1mP3mHjefLciAW-5X__rbAim7rxQ__sYQMWF_7tBf36Q/edit'
+  },
+  {  
+    id:8,
+    facilitatorGuideUrl: 'https://docs.google.com/document/d/1qI6IRSZSlSRPk5oagEoNLLZGTsX7MlHnaUaHmktAdyQ/edit',
+    lessonNotesUrl:'https://docs.google.com/document/d/1qI6IRSZSlSRPk5oagEoNLLZGTsX7MlHnaUaHmktAdyQ/edit'
+  },
+  {  
+    id:10,
+    facilitatorGuideUrl: '',
+    lessonNotesUrl:''
+  },
+  {  
+    id:9,
+    facilitatorGuideUrl: '',
+    lessonNotesUrl:''
+  },
+  {  
+    id:5,
+    facilitatorGuideUrl: 'https://docs.google.com/document/d/1mP3mHjefLciAW-5X__rbAim7rxQ__sYQMWF_7tBf36Q/edit',
+    lessonNotesUrl:'https://docs.google.com/document/d/1mP3mHjefLciAW-5X__rbAim7rxQ__sYQMWF_7tBf36Q/edit'
+  }
+]
+
+export default facilitatorGuideList;
