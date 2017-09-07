689a881b0a5c24eb90314c430bc7aa44a1a38bc8
Morolake Orekoya <morolake.orekoya@andela.com>
2017-09-07 13:52:33 +0100

    allow uses to see available facilitator guide and lesson notes

--- src/js/components/curriculum/GuideNotes.js
+++ src/js/components/curriculum/GuideNotes.js
@@ -1,25 +1,32 @@
 import React, {PropTypes} from 'react';
 import facilitatorGuideList from './facilitatorGuideList'
 
-const GuideNotes = ({session}) =>
-  <div>
-    <p>
-      <a
-        target="_blank"
-        href={facilitatorGuideList.find((element) => element.id === session).facilitatorGuideUrl || '#'}
-      >
-        Facilitator Guide
-      </a>
-    </p>
-    <p>
-      <a
-        target="_blank"
-        href={facilitatorGuideList.find((element) => element.id === session).lessonNotesUrl || '#'}
-      >
-        Lesson Notes
-      </a>
-    </p>
-  </div>
+const GuideNotes = ({session}) => {
+  const sessionInfo = facilitatorGuideList.find((element) => element.id === session)
+  const defaultLink = `#session${session}`
+
+  return (
+    <div>
+      <div className="guides-notes-divider" />
+      <p>
+        <a
+          target={sessionInfo && "_blank"}
+          href={sessionInfo ? sessionInfo.facilitatorGuideUrl : defaultLink}
+        >
+        <i className="fa fa-chevron-right" aria-hidden="true"></i> Facilitator Guide
+        </a>
+      </p>
+      <p>
+        <a
+          target={sessionInfo && "_blank"}
+          href={sessionInfo ? sessionInfo.lessonNotesUrl : defaultLink}
+        >
+        <i className="fa fa-chevron-right" aria-hidden="true"></i> Lesson Notes
+        </a>
+      </p>
+    </div>
+  )
+}
 
 GuideNotes.propTypes = {
   session: PropTypes.number
--- src/js/components/curriculum/facilitatorGuideList.js
+++ src/js/components/curriculum/facilitatorGuideList.js
@@ -5,11 +5,6 @@ const facilitatorGuideList = [
     lessonNotesUrl:'https://docs.google.com/document/d/1sk_zXANIGQ4RtcUg8tPDIN8DRTGYRI0BVFD7yiJarhY'
   },
   {  
-    id:3,
-    facilitatorGuideUrl: '',
-    lessonNotesUrl:''
-  },
-  {  
     id:7,
     facilitatorGuideUrl: 'https://docs.google.com/document/d/1mP3mHjefLciAW-5X__rbAim7rxQ__sYQMWF_7tBf36Q/edit',
     lessonNotesUrl:'https://docs.google.com/document/d/1mP3mHjefLciAW-5X__rbAim7rxQ__sYQMWF_7tBf36Q/edit'
@@ -18,16 +13,6 @@ const facilitatorGuideList = [
     id:8,
     facilitatorGuideUrl: 'https://docs.google.com/document/d/1qI6IRSZSlSRPk5oagEoNLLZGTsX7MlHnaUaHmktAdyQ/edit',
     lessonNotesUrl:'https://docs.google.com/document/d/1qI6IRSZSlSRPk5oagEoNLLZGTsX7MlHnaUaHmktAdyQ/edit'
-  },
-  {  
-    id:10,
-    facilitatorGuideUrl: '',
-    lessonNotesUrl:''
-  },
-  {  
-    id:9,
-    facilitatorGuideUrl: '',
-    lessonNotesUrl:''
   },
   {  
     id:5,
