50c0eed1a10b4ea448ac1e1851f16807c4018592
Chineze <chineze.nwosu@andela.com>
2017-09-11 11:55:38 +0100

    add socials links and next links to page

--- src/js/components/curriculum/FacilitatorGuide.js
+++ src/js/components/curriculum/FacilitatorGuide.js
@@ -4,6 +4,7 @@ import DocumentTitle from '../common/DocumentTitle';
 import dummyData from './guideDummyData';
 import jquery from 'jquery';
 import { sanitizeHtml, joinHtmlItemsWithCommaWithAnd } from '../../utils/helpers';
+import SocialLinks from '../common/SocialLinks';
 
 export class FacilitatorGuidePage extends React.Component {
   constructor(props) {
@@ -45,18 +46,25 @@ export class FacilitatorGuidePage extends React.Component {
   }
 
   render () {
+    const shareProps = {
+      url: window.location.href,
+      title: dummyData[0].title || '',
+      imageUrl: '',
+      description: ''
+    };
+
     return (
       <section className="facilitor-guide">
         <div className="header-content">
           <div className="row">
             <div className="col-md-12">
-              <h3>{dummyData[0].topic}</h3>
-              <div className="col-md-6 text-sm-right header-content-titles">
+              <h3>{dummyData[0].title}</h3>
+              <div className="col-xs-6 text-sm-right header-content-titles">
                 <p>Level</p>
                 <p>Session</p>
                 <p>Authors</p>
               </div>
-              <div className="col-md-6">
+              <div className="col-xs-6 header-content-body">
                 <p>{dummyData[0].level}</p>
                 <p>{dummyData[0].session}</p>
                 <p dangerouslySetInnerHTML={{__html: sanitizeHtml(joinHtmlItemsWithCommaWithAnd(dummyData[0].authors.map(author => author.full_name)))}} />
@@ -72,12 +80,26 @@ export class FacilitatorGuidePage extends React.Component {
               </ul>
             </aside>
             <div className="col-md-12 guide-body">
-              <div className="embed">
-                <div className="embed-responsive embed-responsive-16by9">
-                  <iframe className="embed-responsive-item" src={dummyData[0].intro_video} allowFullScreen></iframe>
+              {
+                dummyData[0].intro_video &&
+                <div className="embed">
+                  <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src={dummyData[0].intro_video} allowFullScreen></iframe>
+                  </div>
+                </div>
+              }
+              <div className="" dangerouslySetInnerHTML={{__html: sanitizeHtml(dummyData[0].body)}} />
+              <SocialLinks {...shareProps} />
+              <div className="col-md-12 next-prev-links">
+                <div className="col-md-5 text-md-right">
+                  <p>Previous:</p>
+                  <a href={dummyData[0].prev_post.link}>{`<< ${dummyData[0].prev_post.title}`}</a>
+                </div>
+                <div className="col-md-5 offset-md-2 text-xs-right text-md-left">
+                  <p>Next:</p>
+                  <a href={dummyData[0].next_post.link}>{`${dummyData[0].next_post.title} >>`}</a>
                 </div>
               </div>
-              <div className="" dangerouslySetInnerHTML={{__html: sanitizeHtml(dummyData[0].body)}} />
             </div>
           </div>
         </div>
