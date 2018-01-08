9b98508af28657096e5151defbcfb494fd533f88
Morolake Orekoya <morolake.orekoya@andela.com>
2018-01-08 14:27:00 +0100

    fix failing blog route test

--- tools/tests/routeTest.js
+++ tools/tests/routeTest.js
@@ -30,7 +30,7 @@ module.exports = (app) => {
     });
 
     it('should return html with meta tags on blog route', (done) => {
-      api.get('/blog/no-blog/a-certain-blog').expect(200).end((err, res) => {
+      api.get('/blogs/no-blog/a-certain-blog').expect(200).end((err, res) => {
         expect(res.text).to.contain('<meta id="og-title"');
         expect(res.text).to.contain('<meta id="meta-description"');
         expect(res.text).to.contain('<meta id="og-url"');
@@ -49,18 +49,18 @@ module.exports = (app) => {
 
     it('should return the index page for every other route', (done) => {
       api.get('/all-other-route').expect(200).end((err, res) => {
-        expect(res.text).to.not.contain('<meta id="og-title"');
-        expect(res.text).to.not.contain('<meta id="meta-description"');
-        expect(res.text).to.not.contain('<meta id="og-url"');
-        expect(res.text).to.not.contain('<meta id="og-image"');
-        expect(res.text).to.not.contain('<meta id="og-type"');
-        expect(res.text).to.not.contain('<meta name="description"');
-        expect(res.text).to.not.contain('<meta name="twitter:card"');
-        expect(res.text).to.not.contain('<meta name="twitter:site"');
-        expect(res.text).to.not.contain('<meta name="twitter:domain"');
-        expect(res.text).to.not.contain('<meta name="twitter:title"');
-        expect(res.text).to.not.contain('<meta name="twitter:description"');
-        expect(res.text).to.not.contain('<meta name="twitter:image"');
+        expect(res.text).to.contain('<meta id="og-title"');
+        expect(res.text).to.contain('<meta id="meta-description"');
+        expect(res.text).to.contain('<meta id="og-url"');
+        expect(res.text).to.contain('<meta id="og-image"');
+        expect(res.text).to.contain('<meta id="og-type"');
+        expect(res.text).to.contain('<meta name="description"');
+        expect(res.text).to.contain('<meta name="twitter:card"');
+        expect(res.text).to.contain('<meta name="twitter:site"');
+        expect(res.text).to.contain('<meta name="twitter:domain"');
+        expect(res.text).to.contain('<meta name="twitter:title"');
+        expect(res.text).to.contain('<meta name="twitter:description"');
+        expect(res.text).to.contain('<meta name="twitter:image"');
         done();
       });
     });
