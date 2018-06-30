2297d425b0b8c8c4b76b24fac99caabd65e5f4dd
Patrick <patrick.luboobi@andela.com>
2018-06-30 22:57:29 +0300

    feat() add reason_for_applying field to application form

--- src/js/components/partner_leads/application/ApplicationForm.js
+++ src/js/components/partner_leads/application/ApplicationForm.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import logo from '../../../../img/logo-colored.png';
 import TextField from '../../common/form/TextField';
+import TextArea from '../../common/form/Textarea';
 import Submit from '../../common/form/Submit';
 
 class ApplicationForm extends React.Component {
@@ -43,6 +44,12 @@ class ApplicationForm extends React.Component {
                 type="tel"
                 name="phone_number"
                 label="Your phone number"
+                onChange={this.props.onChange}
+            />
+            <TextArea
+                placeholder="Reason for applying"
+                name="reason_for_applying"
+                label="Reason for applying"
                 onChange={this.props.onChange}
             />
             <Submit
